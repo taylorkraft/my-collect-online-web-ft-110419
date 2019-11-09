37cf113888625648ad9332bf8cd62bbd1b681b60
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-09 00:38:06 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(collection)
+  x = 0
+  new_collection = []
+  
+  while x < collection.length
+  new_collection.push yield (collection[x])
+  x += 1 
+end
+new_collection
+end
+  
